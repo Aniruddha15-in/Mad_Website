f738b452e6c277d0331496a239382b4048b5f0a6
Saksham Sehgal <sakshamsehgal43@gmail.com>
2024-01-12 17:49:42 +0530

    navbar-done

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Navbar from "./components/Navbar";
 import {Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Accolades from "./pages/Accolades"; 
@@ -5,11 +6,13 @@ import Artists from "./pages/Artists";
 import About from "./pages/About";
 import Doremipa from "./pages/Doremipa";
 import Events from "./pages/Events";
+import "./App.css";
+
 
 function App() {
   return (
     <div>
-      <About />
+      <Navbar />
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Link, useNavigate } from "react-router-dom";
+import navLogo from "../assets/logo-nav.png";
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='navbar'>
+      <div className='navbar-sec1'>
+        <img src={navLogo} />
+        <Link to="/" className='navbar-sec1-link'>MADHURIMA</Link>
+      </div>
+      <div className='navbar-sec2'>
+        <Link to="/doremipa" className='navbar-link'><p>Do Re Mi Pa</p></Link>
+        <Link to="/accolades" className='navbar-link'><p>Accolades</p></Link>
+        <Link to="/artist-spotlight" className='navbar-link'><p>Artist Spotlight</p></Link>
+        <Link to="/events-performances" className='navbar-link'><p>Events/Performances</p></Link>
+        <Link to="/about-us" className='navbar-link'><p>About Us</p></Link>
+      </div>
     </div>
   )
 }

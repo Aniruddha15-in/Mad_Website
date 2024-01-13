d48b47e95a4ed6c87aaa838e0b2d429dcd2baf74
Saksham Sehgal <sakshamsehgal43@gmail.com>
2024-01-14 01:48:51 +0530

    homePage done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import about from "../assets/home-about.png";
 import hero from "../assets/440-home.jpg";
 import sponsor1 from "../assets/sponsor-logo1.png";
@@ -6,54 +6,61 @@ import sponsor2 from "../assets/sponsor-logo2.png";
 import sponsor3 from "../assets/sponsor-logo3.png";
 import sponsor4 from "../assets/sponsor-logo4.png";
 import sponsor5 from "../assets/sponsor-logo5.png";
+import IGcomponent from "../components/IGcomponent";
 
-var perf =require('../components/ig.html');
 
 const Home = () => {
   return (
-    <div className='homePage'>
-      <div className='hero-home'>
+    <div className="homePage">
+      <div className="hero-home">
         <img src={hero} />
       </div>
-      <div className='about-home'>
-          <div>
-            <img src={about} />
-          </div>
-          <div className='content-home'>
-            <p className='p1content-home'>About</p>
-            <p className='p2content-home'>Do Re Mi Pa</p>
-            <p className='p3content-home'>
-              DoReMiPa is the official annual inter-college music competition of Delhi Technological University(DTU) organized by Madhurima - the music society of DTU, which is held under ENGIFEST- the largest cultural fest of north India. 
-              DoReMiPa includes six musical competitions that  showcase a diverse array of musical styles, encompassing Indian, Western, and Instrumental genres and draws participation from all major colleges in the Delhi NCR as well as other parts of India. The competition is judged by professional musicians who are well established within their respective domains.
-            </p>
-            <button className='but-home'>Learn More</button>
-          </div>
-      </div>
-      <div className='sponsors'>
-        <p className='content-sponsors'>Sponsors</p>
-        <div className='current-sponsors'>
-          <img src={sponsor1} className='current-sponsor'/>
-          <img src={sponsor2} className='current-sponsor'/>
-          <img src={sponsor3} className='current-sponsor'/>
-          <img src={sponsor4} className='current-sponsor'/>
-          <img src={sponsor5} className='current-sponsor'/>
+      <div className="about-home">
+        <div>
+          <img src={about} />
+        </div>
+        <div className="content-home">
+          <p className="p1content-home">About</p>
+          <p className="p2content-home">Do Re Mi Pa</p>
+          <p className="p3content-home">
+            DoReMiPa is the official annual inter-college music competition of
+            Delhi Technological University(DTU) organized by Madhurima - the
+            music society of DTU, which is held under ENGIFEST- the largest
+            cultural fest of north India.  DoReMiPa includes six musical
+            competitions that  showcase a diverse array of musical styles,
+            encompassing Indian, Western, and Instrumental genres and draws
+            participation from all major colleges in the Delhi NCR as well as
+            other parts of India. The competition is judged by professional
+            musicians who are well established within their respective domains.
+          </p>
+          <button className="but-home">Learn More</button>
         </div>
       </div>
-      <div className='sponsors'>
-        <p className='content-sponsors'>Past Sponsors</p>
-        <div className='current-sponsors'>
-          <img src={sponsor1} className='current-sponsor'/>
-          <img src={sponsor2} className='current-sponsor'/>
-          <img src={sponsor3} className='current-sponsor'/>
-          <img src={sponsor4} className='current-sponsor'/>
-          <img src={sponsor5} className='current-sponsor'/>
+      <div className="sponsors">
+        <p className="content-sponsors">Sponsors</p>
+        <div className="current-sponsors">
+          <img src={sponsor1} className="current-sponsor" />
+          <img src={sponsor2} className="current-sponsor" />
+          <img src={sponsor3} className="current-sponsor" />
+          <img src={sponsor4} className="current-sponsor" />
+          <img src={sponsor5} className="current-sponsor" />
         </div>
       </div>
-      <div className='ig-embed'>
-        <iframe src={perf}></iframe>
+      <div className="sponsors">
+        <p className="content-sponsors">Past Sponsors</p>
+        <div className="current-sponsors">
+          <img src={sponsor1} className="current-sponsor" />
+          <img src={sponsor2} className="current-sponsor" />
+          <img src={sponsor3} className="current-sponsor" />
+          <img src={sponsor4} className="current-sponsor" />
+          <img src={sponsor5} className="current-sponsor" />
+        </div>
+      </div>
+      <div className="ig-embed">
+        <IGcomponent />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;

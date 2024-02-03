cf18976b17e5d035a9d39b723e214bd337fc7fb7
Saksham Sehgal <sakshamsehgal43@gmail.com>
2024-02-03 19:53:01 +0530

    changed

--- src/components/AccoladesCard.js
+++ src/components/AccoladesCard.js
@@ -1,28 +1,9 @@
 import React, {useState} from 'react'
-import ReactCSSTransitionGroup from 'react-transition-group';
 const AccoladesCard = ({ image, name, tag, wins }) => {
-  
-  const [isShown, setIsShown] = useState(false);
 
   return (
     
     <div className='accoladesCard'>
-      {/* <div 
-        onMouseEnter={() => setIsShown(true)}
-        onMouseLeave={() => setIsShown(false)}
-      >
-        {isShown ? <div className='accHover'>
-          <p>
-            <ReactCSSTransitionGroup
-              transitionName="example"
-              transitionEnterTimeout={500}
-              transitionLeaveTimeout={300}>
-              {wins}
-            </ReactCSSTransitionGroup>
-          </p>
-        </div> : <img src={image} /> }
-      </div> */}
-      
       <img src={image} className='acc-img' />
       <div className='accHover'>{wins}</div>
       <div>
